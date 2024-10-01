636728920ad3872117c59cd38072e51b656de90a
Serhat <serhatdenli7@gmail.com>
2024-10-01 20:47:41 +0300

    .

--- Appointments.Application/MediatR/Responses/UserReponses/GetAllUsersResponse.cs
+++ Appointments.Application/MediatR/Responses/UserReponses/GetAllUsersResponse.cs
@@ -5,9 +5,8 @@ namespace Appointments.Application.MediatR.Responses.UserReponses
 {
     public class GetAllUsersResponse : MediatRBaseResponse
     {
-        [JsonPropertyName("users")]
+
         public List<User> Users { get; set; }
-        [JsonPropertyName("count")]
         public int Count { get; set; }
     }
 }
