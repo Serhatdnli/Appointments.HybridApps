415acd01fd4ddfd1cb90e642be9255c41a7e6d91
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-10-02 21:40:45 +0300

    Added cretepersonal page

---
+++ Appointments.Utility/ObjectWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+namespace Appointments.Utility
+{
+	public static class ObjectWriter
+	{
+		public static void Write(object obj)
+		{
+			if (obj == null)
+			{
+				Console.WriteLine("Null object provided.");
+				return;
+			}
+
+			string json = JsonSerializer.Serialize(obj);
+			Console.WriteLine(json);
+		}
+	}
+}
---
+++ Appointments.Web/Pages/Admin/CreatePersonal.razor.cs
@@ -0,0 +1,50 @@
+﻿using Appointments.Application.MediatR.Requests.UserRequests;
+using Appointments.Application.MediatR.Responses.UserReponses;
+using Appointments.Domain.Enums;
+using Appointments.Domain.Models;
+using Appointments.Utility;
+using BlazorBootstrap;
+using Microsoft.AspNetCore.Components;
+
+namespace Appointments.Web.Pages.Admin
+{
+    public partial class CreatePersonal : ComponentBase
+	{
+        private User User { get; set; } = new User();
+		private List<UserRoleType> RoleOptions => Enum.GetValues(typeof(UserRoleType)).Cast<UserRoleType>().ToList();
+		private async Task CreateAsync()
+        {
+            //User.CreateDate = DateTime.Now;
+            //CreateUserRequest request = new CreateUserRequest
+            //{
+            //    User = User,
+            //};
+
+            try
+            {
+                //var response = await NetworkManager.SendAsync<CreateUserRequest, CreateUserResponse>(request);
+                CreateToastMessage(ToastType.Primary, "Başarılı Şekilde Eklendi");
+            }
+            catch (Exception e)
+            {
+
+                CreateToastMessage(ToastType.Primary, e.ToString());
+                throw;
+            }
+
+            //ObjectWriter.Write(User);
+        }
+
+
+
+        private ToastMessage CreateToastMessage(ToastType toastType, string text)
+        => new ToastMessage
+        {
+            Type = toastType,
+            Title = "Blazor Bootstrap",
+            HelpText = $"{DateTime.Now}",
+            Message = text,
+        };
+
+    }
+}
