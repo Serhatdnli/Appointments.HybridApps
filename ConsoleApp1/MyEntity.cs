55375be38e0e8d5d2af485ba0200e6aa0d603853
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-10-04 15:29:39 +0300

    added test peoject and list extension

---
+++ ConsoleApp1/MyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public Role Role { get; set; }
+    }
+
+}
