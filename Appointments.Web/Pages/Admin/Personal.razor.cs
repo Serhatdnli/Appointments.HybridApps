24f79c1fb816a072ddec34f7280df4d22d76dfbf
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-10-17 15:21:50 +0300

    Doctor page appointment grid added

---
+++ Appointments.Web/Pages/Admin/Personal.razor.cs
@@ -0,0 +1,26 @@
+﻿using Appointments.Domain.Dtos.AppointmentDtos;
+using Appointments.Domain.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Appointments.Web.Pages.Admin
+{
+	public partial class Personal : ComponentBase
+	{
+		[Parameter]
+		public string Id { get; set; }
+
+
+		private List<GetAppointmentDto> appointments { get; set; } = new();
+
+		private GetAppointmentDto tempAppointment { get; set; }
+		private DateTime orderDate = DateTime.Today.AddHours(9);
+
+
+
+		protected async override Task OnInitializedAsync()
+		{
+			Console.WriteLine("id : " + Id);
+
+		}
+	}
+}
